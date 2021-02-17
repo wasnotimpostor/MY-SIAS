5fcae10eb096a7136be4e59985dba4ce5c0c300f
wasnotimpostor <evan.mahendra95@gmail.com>
2021-02-17 14:51:35 +0700

    update jurusan

--- sekolah/src/main/java/sistem/akademik/sekolah/model/Jurusan.java
+++ sekolah/src/main/java/sistem/akademik/sekolah/model/Jurusan.java
@@ -12,6 +12,8 @@ public class Jurusan {
 
     private String name;
 
+    private Long id_matpel;
+
     public Long getId() {
         return id;
     }
@@ -26,5 +28,25 @@ public class Jurusan {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId_matpel() {
+        return id_matpel;
+    }
+
+    public void setId_matpel(Long id_matpel) {
+        this.id_matpel = id_matpel;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "id_matpel", referencedColumnName = "id", insertable = false, updatable = false)
+    private MatPel matPel;
+
+    public MatPel getMatPel() {
+        return matPel;
+    }
+
+    public void setMatPel(MatPel matPel) {
+        this.matPel = matPel;
     }
 }
