f6fa7e14db10d64dec1e03920fde0f667fb32471
wasnotimpostor <evan.mahendra95@gmail.com>
2021-02-17 14:53:10 +0700

    update materi

--- sekolah/src/main/java/sistem/akademik/sekolah/model/Materi.java
+++ sekolah/src/main/java/sistem/akademik/sekolah/model/Materi.java
@@ -12,8 +12,6 @@ public class Materi {
 
     private String deskripsi;
 
-    private Long id_matpel;
-
     public Long getId() {
         return id;
     }
@@ -28,25 +26,5 @@ public class Materi {
 
     public void setDeskripsi(String deskripsi) {
         this.deskripsi = deskripsi;
-    }
-
-    public Long getId_matpel() {
-        return id_matpel;
-    }
-
-    public void setId_matpel(Long id_matpel) {
-        this.id_matpel = id_matpel;
-    }
-
-    @ManyToOne
-    @JoinColumn(name = "id_matpel", referencedColumnName = "id", insertable = false, updatable = false)
-    private MatPel matpel;
-
-    public MatPel getMatpel() {
-        return matpel;
-    }
-
-    public void setMatpel(MatPel matpel) {
-        this.matpel = matpel;
     }
 }
