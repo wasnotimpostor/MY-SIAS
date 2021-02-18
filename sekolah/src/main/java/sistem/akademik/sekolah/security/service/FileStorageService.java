bfa95c913c3400234bd870bebce394254f384b89
wasnotimpostor <evan.mahendra95@gmail.com>
2021-02-18 10:21:23 +0700

    image service

---
+++ sekolah/src/main/java/sistem/akademik/sekolah/security/service/FileStorageService.java
@@ -0,0 +1,39 @@
+package sistem.akademik.sekolah.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import sistem.akademik.sekolah.model.FileDB;
+import sistem.akademik.sekolah.repository.FileDBRepository;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+@Service
+public class FileStorageService {
+
+    @Autowired
+    private FileDBRepository fileDBRepository;
+
+    public FileDB store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+
+        return fileDBRepository.save(FileDB);
+    }
+
+    public FileDB getFile(String id) {
+        return fileDBRepository.findById(id).get();
+    }
+
+    public Stream<FileDB> getAllFiles() {
+        return fileDBRepository.findAll().stream();
+    }
+
+    public FileDB deleteFile(String id) {
+        FileDB fileDB = fileDBRepository.findById(id).orElse(null);
+        fileDBRepository .deleteById(id);
+        return fileDB;
+    }
+}
