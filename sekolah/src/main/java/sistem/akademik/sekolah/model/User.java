23a35fbff8acd393fdb5a238ca90c10cfc084aeb
wasnotimpostor <evan.mahendra95@gmail.com>
2021-03-03 11:29:26 +0700

    update user

--- sekolah/src/main/java/sistem/akademik/sekolah/model/User.java
+++ sekolah/src/main/java/sistem/akademik/sekolah/model/User.java
@@ -1,6 +1,16 @@
 package sistem.akademik.sekolah.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.HashSet;
 import java.util.Set;
 
