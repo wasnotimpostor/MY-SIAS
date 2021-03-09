69e4fe5ee38af2ba745d8aa7db6ea13a4a8141b7
wasnotimpostor <evan.mahendra95@gmail.com>
2021-03-09 11:53:49 +0700

    update sort jadwal murid

--- sekolah/src/main/java/sistem/akademik/sekolah/repository/JadwalMuridRepo.java
+++ sekolah/src/main/java/sistem/akademik/sekolah/repository/JadwalMuridRepo.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sistem.akademik.sekolah.model.JadwalMurid;
 
+import java.util.List;
+
 @Repository
 public interface JadwalMuridRepo extends JpaRepository<JadwalMurid, Long> {
+
+    List<JadwalMurid> findByOrderById_guruAsc();
 }
